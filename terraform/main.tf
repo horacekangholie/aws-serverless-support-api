5018e5d62b9ab46222676fcbd9f917fdab8f0e2a
horacekangholie <horacekangholie@gmail.com>
2026-05-04 15:25:07 +0800

    Minor edit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,9 @@
 # Root module entry point.
 # Resources are organized by concern:
+# - locals.tf
 # - dynamodb.tf
+# - cloudwatch.tf
 # - iam.tf
 # - lambda.tf
 # - api_gateway.tf
-# - cloudwatch.tf
+# - outputs.tf
